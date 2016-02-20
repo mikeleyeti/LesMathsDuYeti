28dbe07fae59713748b17b6525183d5a1cfadb74
MikeLeYeti <mikeleyeti@yahoo.fr>
2016-02-20 13:35:07 +0100

    V 2.1     -Classe Don.     -Partie admin mise à jour pour créer les dons.

--- FCPE_Dons/models.py
+++ FCPE_Dons/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+import datetime
 
 
 # Create your models here.
@@ -11,9 +13,9 @@ class Membre(models.Model):
         ('F', 'Madame'),
         ('f', 'Mademoiselle'),
     )
-    civilité = models.CharField(max_length=1,choices=GENRES,default='H')
-    nom = models.CharField(max_length=30,blank=False)
-    prénom = models.CharField(max_length=30,blank=False)
+    civilité = models.CharField(max_length=1, choices=GENRES, default='H')
+    nom = models.CharField(max_length=30, blank=False)
+    prénom = models.CharField(max_length=30, blank=False)
     CATEGORIES_MEMBRES = (
         ('P', "Président de l'A.P.E de Chaingy"),
         ('V', "Vice-Président de l'A.P.E de Chaingy"),
@@ -33,4 +35,22 @@ class Don(models.Model):
     """
     Classe définissant le don
     """
+    desciption_du_don = models.CharField(max_length=500, blank=False,verbose_name="Description du don")
+    membre = models.ForeignKey('Membre')
+    date_du_don = models.DateField(auto_now=True)
+    nom_entreprise = models.CharField(max_length=50, blank=False,verbose_name="Nom de l'entreprise")
+    nom_contact = models.CharField(max_length=50, blank=True,verbose_name='Nom du contact (facultatif), préciser la civilité, exemple : Madame Bidule ou Monsieur Truc')
+    rue_du_contact = models.CharField(max_length=300, blank=False,verbose_name='Rue')
+    cp_du_contact = models.PositiveIntegerField(validators=[MaxValueValidator(99999)], blank=False,verbose_name='Code Postal')
+    ville_contact = models.CharField(max_length=50, blank=False,verbose_name='Ville')
+    tel_du_contact = models.CharField(max_length=20, blank=True,verbose_name='Téléphone du contact (Facultatif)')
+    email_du_contact = models.EmailField(max_length=254, blank=True,verbose_name='E-mail du contact (Facultatif)')
+    def __str__(self):
+        """
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que
+        nous traiterons plus tard et dans l'administration
+        """
+        return self.desciption_du_don
+
 
