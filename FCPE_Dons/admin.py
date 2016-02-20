bccaf1e86af2409a8554cb61f0ad24c7020d5bc3
MikeLeYeti <mikeleyeti@yahoo.fr>
2016-02-20 13:35:07 +0100

    V 2.1     -Classe Don.     -Partie admin mise à jour pour créer les dons.

--- FCPE_Dons/admin.py
+++ FCPE_Dons/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Membre
+from .models import Membre,Don
 
 # Register your models here.
 class MembreAdmin(admin.ModelAdmin):
@@ -8,4 +8,11 @@ class MembreAdmin(admin.ModelAdmin):
    ordering       = ('nom', )
    search_fields  = ('nom', 'prénom')
 
+class DonAdmin(admin.ModelAdmin):
+   list_display   = ('desciption_du_don','membre','date_du_don','nom_contact')
+   list_filter    = ('membre','date_du_don',)
+   ordering       = ('desciption_du_don', )
+   search_fields  = ('membre','desciption_du_don')
+
 admin.site.register(Membre,MembreAdmin)
+admin.site.register(Don,DonAdmin)
